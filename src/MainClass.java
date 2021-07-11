0ae06381f893a362682f3a4ac9c79797f1d47aab
Alexander <a.v.chumak88@gmail.com>
2021-07-12 00:35:24 +0300

    Решение Тест 3

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,6 +2,16 @@ import org.junit.Test;
 
 public class MainClass
 {
+    //Код Теста 3 Старт
+    private String class_string = "Hello, word";
+
+    public String getClassString()
+    {
+        return class_string;
+    }
+    //Код Теста 3 Конец
+
+
     //Код теста 2 Старт
     private int class_number = 20;
 
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,8 +1,20 @@
+import com.sun.deploy.util.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Locale;
+
 public class MainClassTest extends MainClass
 {
+    //Код Теста 3 Старт
+    @Test
+    public void testGetClassString()
+    {
+        Assert.assertTrue("The string doesnt contain either Hello or hello",getClassString().toLowerCase().contains("hello"));
+    }
+    //Код Теста 3 Конец
+
+
     //Код теста 2 Старт
     @Test
     public void testGetClassNumber()

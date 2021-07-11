028280bcbd7a76229edb1eb689ca4b8aef8b24d1
Alexander <a.v.chumak88@gmail.com>
2021-07-11 23:35:40 +0300

    Решение Тест 2

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,9 +2,20 @@ import org.junit.Test;
 
 public class MainClass
 {
+    //Код теста 2 Старт
+    private int class_number = 20;
+
+    public int getClass_number()
+    {
+        return class_number;
+    }
+    //Код теста 2 Конец
+
+
+    //Код теста 1
     public int getLocalNumber()
     {
         return 14;
     }
-
+    //Код теста 1 Конец
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -3,6 +3,16 @@ import org.junit.Test;
 
 public class MainClassTest extends MainClass
 {
+    //Код теста 2 Старт
+    @Test
+    public void testGetClassNumber()
+    {
+        Assert.assertTrue("20 less than 45",getClass_number() > 45);
+    }
+    //Код теста 2 Конец
+
+
+    //Код теста 1 Старт
     @Test
     public void testGetLocalNumber()
     {
@@ -10,4 +20,5 @@ public class MainClassTest extends MainClass
         int expected = 14;
         Assert.assertEquals("Method getLocalNumber return not 14",expected, actual);
     }
+    //Код теста 1 Конец
 }

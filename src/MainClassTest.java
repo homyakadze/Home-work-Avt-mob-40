10300b89d95192c42ddf0f550d54c2b29f5cd185
Alexander <a.v.chumak88@gmail.com>
2021-07-11 21:02:34 +0300

    Результаты Тест 1

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -6,9 +6,7 @@ public class MainClassTest extends MainClass
     @Test
     public void testGetLocalNumber()
     {
-        int actual = getLocalNumber();
-        int expected = 14;
-        Assert.assertEquals("Method getLocalNumber return not 14",expected, actual);
+        Assert.assertEquals("Method getLocalNumber return not 14",14, getLocalNumber());
     }
 }
 
